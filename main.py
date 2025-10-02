a2346b1f84ba5b430e2af2ef79109801979fc14d
TiChYang <yangt@educ.college-montreal.qc.ca>
2025-10-02 14:55:44 -0400

    config change

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import random
 
-game = True
-
 borne_minimale = 0
 borne_maximale = 10
+
+def tcy(x,y):
+    choice_dif=input("keep original difficulty? y/n")
+    if choice_dif = "n":
+        input()
+    else :
+        borne_minimale = 0
+        borne_maximale = 10
 chiffre_aleatoire = random.randint(borne_minimale, borne_maximale)
 
 nb_essaie = 0
-nombre_joueur = -1
 play_game = True
 
 while play_game:
     print('Jai choisi un nombre entre 0 et 10')
-    input("Entrer votre nombre: ")
-
-    if nombre_joueur == chiffre_aleatoire:
-        play_game = False
+    answer = int(input("Entrer votre essaie: "))
+    if answer > chiffre_aleatoire:
+        print("number is too big try again: ")
+    elif answer < chiffre_aleatoire:
+        print("number is too small try again: ")
+    else:
+        print("YOU GOT IT")
+        resume = input("do you want to contunue? y/n")
+        if resume == "y":
+            chiffre_aleatoire = random.randint(borne_minimale, borne_maximale)
+        else:
+            play_game = False
+print("See you soon....")
