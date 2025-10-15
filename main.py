4100a77710fd254a5a55f27e7f55848c76382897
TiChYang <yangt@educ.college-montreal.qc.ca>
2025-10-15 14:49:20 -0400

    config change

--- main.py
+++ main.py
@@ -3,21 +3,32 @@ import random
 borne_minimale = 0
 borne_maximale = 10
 
-def tcy(x,y):
-    choice_dif=input("keep original difficulty? y/n")
-    if choice_dif == "n":
-        input()
-    else :
-        borne_minimale = 0
-        borne_maximale = 10
-chiffre_aleatoire = random.randint(borne_minimale, borne_maximale)
+
+def generator(small, big):
+    return random.randint(small, big)
+
+first = input("chose the smallest number")
+last = input("chose the biggest number")
+try:
+    first = int(first)
+    last = int(last)
+except ValueError:
+    print("i dont like that")
+    exit()
+chiffre_aleatoire = generator(first, last)
+
 
 nb_essaie = 0
 play_game = True
 
 while play_game:
-    print('I chose a number bettween 1 and 10')
-    answer = int(input("Entrer votre essaie: "))
+    print(f'I chose a number bettween {first} and {last}')
+    answer = input("Entrer votre essaie: ")
+    try:
+        answer = int(answer)
+    except ValueError:
+        print("i dont like that")
+        continue
     if answer > chiffre_aleatoire:
         print("number is too big try again: ")
     elif answer < chiffre_aleatoire:
@@ -26,7 +37,15 @@ while play_game:
         print("YOU GOT IT")
         resume = input("do you want to continue? y/n")
         if resume == "y":
-            chiffre_aleatoire = random.randint(borne_minimale, borne_maximale)
+            first = input("chose the smallest number")
+            last = input("chose the biggest number")
+            try:
+                first = int(first)
+                last = int(last)
+            except ValueError:
+                print("i dont like that")
+                exit()
+            chiffre_aleatoire = generator(first, last)
         else:
             play_game = False
 print("See you soon....;) ")
